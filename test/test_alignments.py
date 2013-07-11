df2c5138f25109bb406360fb6b10b816f1e2d4bd
Stephen Holsapple <sholsapp@gmail.com>
2013-07-10 23:01:54 -0700

    bio: fixed the tests
    We're off to a rocky start.

--- test/test_alignments.py
+++ test/test_alignments.py
@@ -1,20 +1,16 @@
 from nose.tools import *
 
-from bio import Cost
+from bio import FixedCost
 from bio.alignments import GlobalMatrix, LocalMatrix
 
 def test_global():
   gene1 = 'GGAATGG'
   gene2 = 'ATG'
-  g = GlobalMatrix(gene1, gene2, Cost)
-  g.initialize()
-  g.execute()
+  g = GlobalMatrix(gene1, gene2, FixedCost())
   assert_equal(g._matrix[7][3].value, -5)
 
 def test_local():
   gene1 = 'GGAATGG'
   gene2 = 'ATG'
-  l = LocalMatrix(gene1, gene2, Cost)
-  l.initialize()
-  l.execute()
+  l = LocalMatrix(gene1, gene2, FixedCost())
   assert_equal(l._matrix[6][3].value, 3)
