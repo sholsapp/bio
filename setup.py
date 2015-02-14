fe940dd093e2fd5fa685bfc7a2f5b7ac150d27d8
Stephen Holsapple <sholsapp@gmail.com>
2015-02-14 11:54:50 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.abspath('README.md')) as fh:
 
 setup(
   name='bio',
-  version='0.0.1',
+  version='0.0.2',
   description=README,
   author='Stephen Holsapple',
   author_email='sholsapp@gmail.com',
